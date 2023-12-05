8ce122bb03ca333c63cd2b3ced694d786c2ef270
Lars Bärring <lars.barring@smhi.se>
2023-12-05 10:57:10 +0100

    improvements to paint_text.py

--- BCP-14/paint_text.py
+++ BCP-14/paint_text.py
@@ -1,40 +1,73 @@
 import argparse
+import re
 from glob import glob
-from pathlib import Path
 from os import system
 
- 
+# Order is important:
 KEYWORDS = ["MUST NOT", "SHALL NOT", "SHOULD NOT",
             "MUST", "REQUIRED", "SHALL", "SHOULD",
             "RECOMMENDED", "MAY", "OPTIONAL"
 ]
-# undecided: aqua, 
-# discuss: yellow, 
-# done: lime,
-# agreed: no background color (red text),
-# ready to merge: black text (still no background color)
+# Other words that may be relevant to look at:
+# DEPRECATED, CAN
 
-HIGHLIGHT_LIST = ["red aqua-background", "red yellow-background", "red lime-background", "red"]
-HIGHLIGHT = HIGHLIGHT_LIST[3]
+# Colors according to asciidoctor default setup
+#    no foreground = "black"
+#    no background = "white"
+# code: fore., back.,  use  
+COLOR_DICT = {
+    0: ("red", "aqua", "not worked on"),
+    1: ("red", "lime", "suggested change"),
+    2: ("red", "yellow", "needs discussion"),
+    3: ("red", "", "agreed"),
+    4: ("", "", "ready to merge"),
+}
 
+def adoc_colors(color_code):
+    fg = COLOR_DICT[color_code][0]
+    bg = COLOR_DICT[color_code][1]
+    if fg and bg:
+        color = f"[{fg} {bg}-background]"
+    elif fg:
+        color = f"[{fg}]"
+    else:
+        color = ""
+    return color
 
-def paint(text):
-    for k in KEYWORDS:
-        klower = k.lower()
-        kin = f" {klower} "
-        kout = f" [{HIGHLIGHT}]#{klower}# "
-        text = text.replace(kin, kout)
+def color_help(color_code):
+    fg = COLOR_DICT[color_code][0]
+    bg = COLOR_DICT[color_code][1]
+    if not fg:
+        fg = "black"
+    if not bg:
+        bg = "none"
+    help = COLOR_DICT[color_code][2]
+    text = f'{color_code}={fg}/{bg} [{help}]'
     return text
 
 
-def process_file_list(file_list):
+def paint(text, color_code):
+    color = adoc_colors(color_code)
+    count = 0
+    for k in KEYWORDS:
+        kin = f" {k.lower()} "
+        if color:
+            kout = f" {color}#{k.lower()}# "
+        else:
+            kout = kin
+        text, c = re.subn(kin, kout, text)
+        count += c
+    return text, count
+
+
+def process_file_list(file_list, color_code):
     for file in file_list:
         file_out = f"{file[1:]}"
-        print(f"IN: {file},   OUT: {file_out}")
         with open(file, "r") as fin:
-            text = paint(fin.read())
+            text, count = paint(fin.read(), args.color_code)
             with open(file_out, "w") as fut:
                 fut.write(text)
+        print(f"IN: {file:26}   OUT: {file_out:26} count = {count}")
 
 
 if __name__ == "__main__":
@@ -42,15 +75,16 @@ if __name__ == "__main__":
                         prog="paint_text",
                         description="Paint BCP-14 keywords in .adoc files.")
     parser.add_argument("filename", default="test_ch02.adoc")
+    parser.add_argument("-c", "--color_code",
+                         default=0,
+                         help=", ".join([color_help(c) for c in COLOR_DICT])
+                        )
     args = parser.parse_args()
     file_pattern = args.filename
     if file_pattern == "*":
         file_pattern += ".adoc"
     indir = "../"
-    # print(indir + file_pattern)
     file_list = glob(indir + file_pattern)
-    print(file_list)
-    process_file_list(file_list)
+    process_file_list(file_list, args.color_code)
 
     system('asciidoctor --verbose ${FINAL_TAG} -a docprodtime="$(date -u ${DATE_FMT})" cf-conventions.adoc -D conventions_build')
-
